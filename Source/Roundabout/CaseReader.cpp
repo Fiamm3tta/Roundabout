ca954a78f3d302258a02264a3412da702e538a95
Jihun Park <wlehfdl0817@naver.com>
2026-02-04 14:51:21 +0900

    Render Sentences and RichText Markup

---
+++ Source/Roundabout/CaseReader.cpp
@@ -0,0 +1,53 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "CaseReader.h"
+#include "Components/RichTextBlock.h"
+
+void UCaseReader::SetSentences(const TArray<FRuntimeSentence>& InputSentences)
+{
+    if (!RichText) return;
+
+    const FString Markup = BuildMarkup(InputSentences);
+    RichText->SetText(FText::FromString(Markup));
+}
+
+FString UCaseReader::BuildMarkup(const TArray<FRuntimeSentence>& InputSentences) const
+{
+    FString Output;
+    Output.Reserve(InputSentences.Num() * 32);
+
+    for(int32 i = 0; i < InputSentences.Num(); i++)
+    {
+        FString Sentence = InputSentences[i].Text.ToString();
+        Sentence = EscapeForRichText(Sentence);
+
+        if (Sentence == "")
+        {
+            Output += TEXT("\n\n");
+            continue;
+        }
+
+        Output += TEXT("<s idx=\"");
+        Output += FString::FromInt(i);
+        Output += TEXT("\">");
+        Output += Sentence;
+        Output += TEXT("</s>");
+
+        if(i + 1 < InputSentences.Num())
+        {
+            Output += TEXT(" ");
+        }
+    }
+
+    return Output;
+}
+
+FString UCaseReader::EscapeForRichText(const FString& Input)
+{
+    FString String = Input;
+    String.ReplaceInline(TEXT("&"), TEXT("&amp;"));
+    String.ReplaceInline(TEXT("<"), TEXT("&lt;"));
+    String.ReplaceInline(TEXT(">"), TEXT("&gt;"));
+    return String;
+}
---
+++ Source/Roundabout/CaseReader.h
@@ -0,0 +1,33 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Blueprint/UserWidget.h"
+#include "CaseSubsystem.h"
+#include "CaseReader.generated.h"
+
+/**
+ * 
+ */
+
+class URichTextBlock;
+
+UCLASS()
+class ROUNDABOUT_API UCaseReader : public UUserWidget
+{
+	GENERATED_BODY()
+
+public:
+	UFUNCTION(BlueprintCallable, Category="Make Body Text")
+    void SetSentences(const TArray<FRuntimeSentence>& InSentences);
+	
+protected:
+	UPROPERTY(meta=(BindWidget))
+	URichTextBlock* RichText = nullptr;
+
+private:
+	FString BuildMarkup(const TArray<FRuntimeSentence>& InputSentences) const;
+
+	static FString EscapeForRichText(const FString& Input);
+};
